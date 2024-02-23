b25d0a7ed78bc023c1e4c9b75e90da8d40dba053
Denneson <Zenneson@users.noreply.github.com>
2024-02-23 07:46:40 -0500

    Update package.json and Firebase configuration*** ***Add signout functionality and sign-in with provider functionality

--- app/libs/firebase.js
+++ app/libs/firebase.js
@@ -1,21 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "cre8ive-4mula.firebaseapp.com",
-  projectId: "cre8ive-4mula",
-  storageBucket: "cre8ive-4mula.appspot.com",
-  messagingSenderId: "461648490793",
-  appId: "1:461648490793:web:881641f6e86a8f99568738",
-  measurementId: "G-GJHPEFVQHX",
+  authDomain: "cre8ive-4mula-11d31.firebaseapp.com",
+  projectId: "cre8ive-4mula-11d31",
+  storageBucket: "cre8ive-4mula-11d31.appspot.com",
+  messagingSenderId: "525927454599",
+  appId: "1:525927454599:web:3526f027975b7cda6ae522",
+  measurementId: "G-FGSX08MHCE",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const firestore = getFirestore(app);
-export const storage = getStorage(app);
+// export const firestore = getFirestore(app);
+// export const storage = getStorage(app);
 // export const analytics = getAnalytics(app);
